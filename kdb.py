b80f48a7113f578e57d6703d311a987c55560355
ASDF44 <42999582+ASDF44@users.noreply.github.com>
2020-06-20 20:09:46 +0530

    Update kdb.py

--- kdb.py
+++ kdb.py
@@ -1,3 +1,22 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2018, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+/******************************************************************************
+ *
+ * Copyright (c) 2018, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+    
+
 import tornado
 import tornado.web
 import tornado.websocket
